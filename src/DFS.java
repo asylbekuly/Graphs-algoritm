0d4cfa1df16c97266b471598839b51aef045f10e
Rabat <rabatkarabek8@gmail.com>
2024-05-24 01:27:12 +0500

    DFS

--- src/DFS.java
+++ src/DFS.java
@@ -1,3 +1,18 @@
-public class DFS {
+public class DFS<V> extends Search<V> {
 
+    public DFS(MyGraph<V> graph, V source) {
+        super(source);
+        deep(graph, source);
+    }
+
+    private void deep(MyGraph<V> graph, V source) {
+        marked.add(source);
+
+        for (V value : graph.getVertex(source).getAdjacentVerticesData()) {
+            if (!marked.contains(value)) {
+                edgeTo.put(value, source);
+                deep(graph, value);
+            }
+        }
+    }
 }
