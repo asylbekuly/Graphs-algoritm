12a7062d0c859b2f4dd831ca79d369760aa3da12
Rabat <rabatkarabek8@gmail.com>
2024-05-24 01:15:03 +0500

    Starting Graphs

---
+++ src/Search.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Search<V>{
+    protected V source;
+    protected Map<V, V> edgeTo;
+    protected Set<V> marked;
+
+    public Search(V source) {
+        this.source = source;
+        marked = new HashSet<>();
+        edgeTo = new HashMap<>();
+    }
+
+    public boolean hasPathTo(V value) {
+        return marked.contains(value);
+    }
+
+    public Iterable<V> Path_To(V value) {
+        if (hasPathTo(value) == false) {
+            return null;
+        }
+
+        Stack<V> path = new Stack<>();
+        for (V i = value; i != null; i = edgeTo.get(i)) {
+            path.push(i);
+        }
+
+        return path;
+    }
+
+}
---
+++ src/Vertex.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Vertex<V> {
+    private V data;
+    private Map<Vertex<V> , Double> map = new HashMap<>();
+    public V getData() {
+        return data;
+    }
+    public Vertex(V data) {
+        this.data = data;
+    }
+
+
+    public void setData(V data) {
+        this.data = data;
+    }
+
+    public void addAdjacentVertex(Vertex<V> destination, Double weight) {
+        map.put(destination, weight);
+    }
+
+    public Map<Vertex<V>, Double> getAdjacentVertices() {
+        return map;
+    }
+
+    public Iterable<V> getAdjacentVerticesData() {
+        return map.keySet().stream().map(Vertex::getData)::iterator;
+    }
+
+
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+
+        if (object == null || getClass() != object.getClass()) return false;
+
+        Vertex<?> otherVertex = (Vertex<?>) object;
+
+        return data.equals(otherVertex.data);
+    }
+}
